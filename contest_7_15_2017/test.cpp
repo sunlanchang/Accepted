5fdb53b13d875fcece798ec2a1a0da3547ad8c5b
sunlanchang <sunlanchang@outlook.com>
2017-07-15 22:35:18 +0800

    add files

---
+++ contest_7_15_2017/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "hello VS Code" << endl;
+    // getchar();
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
