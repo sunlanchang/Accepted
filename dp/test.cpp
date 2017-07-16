6506ddbe49a2a16d3b05977589164e1ab3929df6
sunlanchang <sunlanchang@outlook.com>
2017-07-17 01:34:09 +0800

    有条件的01背包

--- dp/test.cpp
+++ dp/test.cpp
@@ -1,34 +1,55 @@
-#include <cstdio>
+#include <iostream>
 #include <cstring>
 #include <algorithm>
-#include <iostream>
+#include <cstdio>
+
 using namespace std;
+
+int wei[1010]; //重量==价值
+int dp[1010][1010];
+int max(int x, int y)
+{
+    if (x >= y)
+    {
+        return x;
+    }
+    else
+        return y;
+}
+
 int main()
 {
-    int n, m, v, i, j, d[50005], a, b;
-    scanf("%d", &n);
-    while (n--)
+    int n, i, j, ians;
+    while (scanf("%d", &n) && n)
     {
-        scanf("%d%d", &m, &v);
-        memset(d, -100, sizeof(d));
-        d[0] = 0;
-        for (i = 0; i < m; i++)
+        memset(dp, 0, sizeof(dp));
+        for (i = 1; i <= n; i++)
         {
-            scanf("%d%d", &a, &b);
-            for (j = a; j <= v; j++)
-            {
-                d[j] = max(d[j - a] + b, d[j]);
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                cout << d[i] << " ";
-            }
-            cout << "\n";
+            scanf("%d", &wei[i]);
         }
-        if (d[v] < 0)
-            printf("NO\n");
-        else
-            printf("%d\n", d[v]);
+        sort(wei + 1, wei + n + 1);
+        scanf("%d", &ians); //卡余额
+        if (ians < 5)
+        {
+            cout << ians << "\n";
+            continue;
+        }
+        for (i = 1; i <= n - 1; i++)
+        {
+            for (j = 0; j <= ians - 5; j++)
+            {
+                if (j >= wei[i])
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wei[i]] + wei[i]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        cout << ians - dp[n - 1][ians - 5] - wei[n] << "\n";
     }
     return 0;
 }
