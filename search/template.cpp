8acac2f58fc59fcb75a085f09ed9242ce4664eb4
sunlanchang <sunlanchang@outlook.com>
2017-07-20 16:48:11 +0800

    搜索

---
+++ search/template.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+#define ONLINE_JUDGE
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    long _begin_time = clock();
+#endif
+
+#ifndef ONLINE_JUDGE
+    long _end_time = clock();
+    printf("time = %ld ms\n", _end_time - _begin_time);
+#endif
+    return 0;
+}
