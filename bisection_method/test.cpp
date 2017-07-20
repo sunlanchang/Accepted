4ecd05d5126242819b1c288e843710809370b6ef
sunlanchang <sunlanchang@outlook.com>
2017-07-20 15:29:48 +0800

    测试模板

--- bisection_method/test.cpp
+++ bisection_method/test.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-#define ONLINE_JUDGE
+// #define ONLINE_JUDGE
 
 int main()
 {
@@ -15,7 +15,12 @@ int main()
     freopen("out.txt", "w", stdout);
     long _begin_time = clock();
 #endif
-
+    for (int i = 0; i < 10; i++)
+    {
+        int a;
+        scanf("%d", &a);
+        printf("%d\n", a);
+    }
 #ifndef ONLINE_JUDGE
     long _end_time = clock();
     printf("time = %ld ms\n", _end_time - _begin_time);
