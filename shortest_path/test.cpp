e7f098c59f36653bdd3b425f8ee8ff4ae6f82699
sunlanchang <sunlanchang@outlook.com>
2017-07-26 23:16:56 +0800

    add test files

--- shortest_path/test.cpp
+++ shortest_path/test.cpp
@@ -1,58 +1,114 @@
 #include <iostream>
-#include <cstring>
+#include <algorithm>
+#include <cmath>
 #include <cstdio>
-#define INF 999999999
 using namespace std;
+//Kruskal
 
-int map[105][105], visit[105], dis[105], M;
-int prim()
+struct Point
 {
-    for (int i = 1; i <= M; i++)
+    int x, y;
+};
+struct Edge
+{
+    int u, v;
+    double w;
+};
+
+const int INF = 0x3f3f3f3f;
+int pre[10010];
+int N; //顶点数目
+int M; //边数目
+Point po[10010];
+
+void init()
+{
+    for (int i = 0; i < 111; i++)
+        pre[i] = i;
+}
+int find(int x)
+{
+    if (pre[x] == x)
+        return x;
+    return pre[x] = find(pre[x]);
+}
+
+void mix(int x, int y)
+{
+    int fx = find(x), fy = find(y);
+    if (fx != fy)
+        pre[y] = fx;
+}
+
+bool cmp(Edge a, Edge b)
+{
+    if (a.w != b.w)
+        return a.w < b.w;
+    if (a.u != b.u)
+        return a.u < b.u;
+    if (a.u != b.v)
+        return a.v < b.v;
+}
+void kruskal()
+{
+    Edge e[10010];
+    int i, j, count;
+    double cost;
+    bool clear;
+    count = 0;
+    for (i = 1; i <= N; i++)
     {
-        dis[i] = map[i][1];
-    }
-    dis[1] = 0;
-    visit[1] = 1;
-    int sum = 0;
-    for (int i = 1; i <= M - 1; i++)
-    {
-        int temp = INF, pos;
-        for (int j = 1; j <= M; j++)
+        for (j = 1; j <= N; j++)
         {
-            if (!visit[j] && temp > dis[j])
-            {
-                temp = dis[j];
-                pos = j;
-            }
+            double jud = sqrt((po[i].x - po[j].x) * (po[i].x - po[j].x) + (po[i].y - po[j].y) * (po[i].y - po[j].y));
+            e[count].u = i;
+            e[count].v = j;
+            if (jud < 10 || jud > 1000)
+                e[count++].w = INF;
+            else
+                e[count++].w = jud;
         }
-        if (temp == INF)
-            return 0;
-        visit[pos] = 1;
-        sum += dis[pos];
-        for (int j = 1; j <= M; j++)
-            if (!visit[j] && map[pos][j] < dis[j] && map[pos][j] != INF)
-                dis[j] = map[pos][j];
     }
-    return sum;
+    // cout << "count: " << count << endl;
+    sort(e + 1, e + count + 1, cmp);
+    cost = 0;
+    for (i = 1; i <= count - 1; i++)
+    {
+        if (e[i].w != INF && find(e[i].u) != find(e[i].v))
+        {
+            mix(e[i].u, e[i].v);
+            cost += e[i].w;
+        }
+    }
+    clear = true;
+    for (i = 2; i <= N; i++)
+    {
+        if (find(1) != find(i))
+        {
+            clear = false;
+            break;
+        }
+    }
+    if (clear)
+        printf("%.1f\n", cost * 100);
+    else
+        printf("oh\n");
 }
 int main()
 {
-    int N;
-    while (scanf("%d%d", &N, &M), N)
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    int i, T; //T组数据
+    scanf("%d", &T);
+    while (T--)
     {
-        int u, v, w;
-        memset(map, 0x3f, sizeof(map));
-        memset(dis, 0x3f, sizeof(dis));
-        memset(visit, 0, sizeof(visit));
-        for (int i = 1; i <= N; ++i)
+        scanf("%d", &N);
+        init();
+        for (i = 1; i <= N; i++)
         {
-            scanf("%d%d%d", &u, &v, &w);
-            map[u][v] = map[v][u] = w;
+            scanf("%d%d", &po[i].x, &po[i].y);
         }
-        int ans = prim();
-        if (ans)
-            printf("%d\n", ans);
-        else
-            printf("?\n");
+        kruskal();
     }
+    return 0;
 }
