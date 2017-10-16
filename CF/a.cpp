ac63203706e640a696a818bd05a05c99c72b65ef
sunlanchang <sunlanchang@outlook.com>
2017-10-16 08:04:15 +0800

    change some thing

--- CF/a.cpp
+++ CF/a.cpp
@@ -7,9 +7,21 @@
 
 using namespace std;
 
+int n, k, t;
+
 int main()
 {
-    freopen("in.txt", "r", stdin);
-
+    // freopen("in.txt", "r", stdin);
+    while (~scanf("%d%d%d", &n, &k, &t))
+    {
+        int ans = 0;
+        if (t <= k)
+            ans = t;
+        else if (t > k && t <= n)
+            ans = k;
+        else
+            ans = k - (t - n);
+        printf("%d\n", ans);
+    }
     return 0;
 }
