48a5effeec9f883377a45a389aca5d3e0be170da
sunlanchang <sunlanchang@outlook.com>
2018-02-04 20:14:59 +0800

    complete code

--- CF/3A.py
+++ CF/3A.py
@@ -1,13 +1,17 @@
 p1, p2 = input(), input()
 x1, y1 = ord(p1[0]) - ord('a') + 1, int(p1[1])
 x2, y2 = ord(p2[0]) - ord('a') + 1, int(p2[1])
-x = x2-x1
-y = y2-y1
+x = x2 - x1
+y = y2 - y1
 d1, d2 = 'L', 'D'
 if x > 0:
     d1 = 'R'
+else:
+    x = -x
 if y > 0:
     d2 = 'U'
+else:
+    y = -y
 if x > y:
     print(x)
 else:
