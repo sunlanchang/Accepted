d3100e58e8f5e951d50537cbe7751cdf1736e05d
sunlanchang <sunlanchang@outlook.com>
2018-03-21 17:17:05 +0800

    集合减法

---
+++ water/hdu2034.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int arr[999999];
+int a[111], b[111], res[111];
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    int m, n;
+    while (~scanf("%d%d", &m, &n) && (m || n))
+    {
+        memset(arr, 0x3f, sizeof(arr));
+        memset(a, 0, sizeof(a));
+        memset(b, 0, sizeof(b));
+        memset(res, 0, sizeof(res));
+        for (int i = 0; i < m; i++)
+            scanf("%d", &a[i]);
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &b[i]);
+            arr[b[i]] = 1;
+        }
+        int length_inc = 0;
+        for (int i = 0; i < m; i++)
+            if (arr[a[i]] != 1)
+                res[length_inc++] = a[i];
+        if (length_inc == 0)
+        {
+            printf("NULL\n");
+            continue;
+        }
+        sort(res, res + length_inc);
+        for (int i = 0; i < length_inc; i++)
+            printf("%d ", res[i]);
+        printf("\n");
+    }
+    return 0;
+}
