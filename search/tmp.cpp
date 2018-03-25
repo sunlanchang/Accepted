fd4506af77b3d720a9d49565d174d0c7c4fd2826
sunlanchang <sunlanchang@outlook.com>
2018-03-25 16:02:03 +0800

    蓝桥杯

--- search/tmp.cpp
+++ search/tmp.cpp
@@ -1,24 +1,15 @@
 #include <cstdio>
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <string>
-#include <cmath>
 using namespace std;
-int a[10];
-struct s
-{
-    int x : 2, y : 3;
-    // s()
-    // {
-    //     x = 0, y = 0;
-    // }
-};
 int main()
 {
-    // freopen("in.txt", "r", stdin);
-    s tmp;
-    cout << tmp.x << endl;
+    freopen("out.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    double money, sum, sale;
+    while (~scanf("%lf %lf", &money, &sale))
+    {
+        sum += money * sale * 0.01;
+    }
+    cout << sum << endl;
     return 0;
 }
